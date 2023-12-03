2b60adfa9f0633173de14b2d0325425191448a38
Arun Tomar <arun@automatehub.com>
2023-12-03 11:00:01 -0500

    deploy argocd with load balancer ip and try path based routing to argocd via istio

--- proxmox/k8s_system_apps/argocd.tf
+++ proxmox/k8s_system_apps/argocd.tf
@@ -10,14 +10,14 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = kubernetes_namespace_v1.argocd.id
-#  set {
-#    name  = "server.service.type"
-#    value = "LoadBalancer"
-#  }
-#  set {
-#    name  = "server.service.loadBalancerIP"
-#    value = "172.17.9.14"
-#  }
+    set {
+      name  = "server.service.type"
+      value = "LoadBalancer"
+    }
+    set {
+      name  = "server.service.loadBalancerIP"
+      value = "172.17.9.14"
+    }
   set {
     name  = "controller.metrics.enabled"
     value = true
@@ -30,4 +30,28 @@ resource "helm_release" "argocd" {
     name  = "server.metrics.enabled"
     value = true
   }
+}
+
+resource "kubectl_manifest" "argo-vs" {
+  yaml_body = <<-YAML
+  apiVersion: networking.istio.io/v1beta1
+  kind: VirtualService
+  metadata:
+    name: argocd-vs
+    namespace: argocd
+  spec:
+    hosts:
+      - "*"
+    gateways:
+      - istio-system/istio-gateway
+    http:
+      - match:
+          - uri:
+              exact: /argocd
+        route:
+          - destination:
+              host: argocd-server.argocd.svc.cluster.local
+              port:
+                number: 443
+  YAML
 }
